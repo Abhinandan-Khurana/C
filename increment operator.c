593ad155e296d9f2efcddbc4b863b45001048868
Abhinandan-Khurana <75041733+Abhinandan-Khurana@users.noreply.github.com>
2020-11-28 18:07:22 +0530

    Create increment operator.c

---
+++ increment operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 2;
+    // printf("++i = %d\n", ++i); pehle incriment akre phir print kare..
+    // printf("i++=%d\n", i++); pehle print kare phir incriment kare..
+    // printf("--i=%d\n", --i); pehle decrement kare phir print kare..
+    // printf("i--=%d\n", i--); pehle print kare phir decrement kare..
+    // i+=10 --> increments i by 10. also -=, *=, /=, %= work.
+    return 0;
+}
